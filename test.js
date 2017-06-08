27af58d3102e84921a7865825ee8385eaad4d1cc
Alex <alexandersuglio@gmail.com>
2017-06-08 13:27:17 -0400

    altered file order in repo

--- test.js
+++
@@ -1,8 +0,0 @@
- for (var key in keysDown) {
-     var value = Number(key);
-     if (value == 37) { //left arrow
-         this.paddle.move(-4, 0);
-     }
-     if (value == 39) { //right arrow
-         this.paddle.move(4, 0);
-     };
